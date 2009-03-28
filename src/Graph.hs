275d2643cab7884598edd298ff1a0deeda4dda0c
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-03-28 00:59:45 -0700

    [Graph] working implementation of polymorphic graphs
    Polymorphics graphs implemented along with the ability to
    add and remove the identity edges.
    
    TODO: QuickCheck should show that addIdentityEdges and removeIdentityEdges
    are opposites.
    
    TEST: defined two graphs 'g1' and 'g2' to mimic those shown on the
    Wikipedia page on the modular product of graphs.

--- src/Graph.hs
+++ src/Graph.hs
@@ -1,56 +1,106 @@
 {-# LANGUAGE
-  ExistentialQuantification, 
-  NamedFieldPuns, 
-  MultiParamTypeClasses,
-  ScopedTypeVariables 
+  ExistentialQuantification ,
+  FlexibleContexts          ,
+  GADTs                     ,
+  MultiParamTypeClasses     ,
+  NamedFieldPuns            ,
+  ParallelListComp          ,
+  ScopedTypeVariables       ,
+  UndecidableInstances
   #-}
 
 module Graph where
 
 import Prelude hiding (id)
-import qualified Data.Map as Map
-import qualified Data.Set as Set
+import Data.List
 
+{---------------------------- Type declarations -------------------------------}
 type ID = Int
 
-data Label l = Label l deriving (Eq, Show)
+data Label l = Label l | None deriving (Eq, Show)
 
-{---------------------------------- Vertex-------------------------------------}
-data Eq v => Vertex v = Vertex {
-      id          :: ID
-    , labelVertex :: Label v } deriving (Show)
 
-instance Eq v => Eq (Vertex v) where
-    v1 == v2 = if id v1 == id v2 && 
-                  labelVertex v1 == labelVertex v2 
-               then True
-               else False
+{---------------------------------- Vertex ------------------------------------}
+data Vertex v = Vertex {
+      id ::ID
+    ,labelV :: Label v
+    } deriving (Eq, Show)
 
-instance Eq v => Ord (Vertex v) where
-    compare v1 v2 = compare (id v1) (id v2)
 
 {---------------------------------- Edge --------------------------------------}
+--data Edge e = Edge (ID,ID) e deriving Show
 data Edge e = Edge {
-      origin    :: ID
-    , target    :: ID
-    , labelEdge :: Label e } deriving (Show)
+      origin :: ID
+    , target :: ID
+    , labelE :: Label e
+    } deriving Show
 
-instance Eq e => Eq (Edge e) where
-    e1 == e2 = if ((origin  e1 == origin    e2 && target e1 == target e2) ||
-                   (origin  e1 == target    e2 && target e1 == origin e2)) &&
-                  labelEdge e1 == labelEdge e2
+instance Eq (Edge e) where
+    e1 == e2 = if (origin e1 == origin e2 && target e1 == target e2) ||
+                  (origin e1 == target e2 && target e1 == origin e2)
                then True
                else False
-
-instance Eq e => Ord (Edge e) where
 
 
 {---------------------------------- Graph -------------------------------------}
 data Graph v e = Graph {
-      vertices :: Set.Set (Vertex v)
-    , edges    :: Set.Set (Edge e) } deriving (Show)
+      vertices :: [Vertex v]
+    , edges    :: [Edge e]
+    } deriving Show
 
-mkGraph :: Set.Set (Vertex v) -> Set.Set (Edge e) -> Graph v e
-mkGraph vs es = Graph {vertices = vs, edges = es}
-mkGraph' :: (Eq e, Eq v) => [Vertex v] -> [Edge e] -> Graph v e
-mkGraph' vs es = mkGraph (Set.fromList vs) (Set.fromList es)
+mkGraph :: [Vertex v] -> [Edge e] -> Graph v e
+mkGraph vs es = Graph vs es
+
+mkGraph' :: [Label v] -> [(ID,ID,Label e)] -> Graph v e
+mkGraph' vs es = mkGraph vs' es'
+    where vs' = snd $ mapAccumL (\acc v -> (acc + 1,Vertex acc v)) 1 vs
+          es' = map (\(o,t,l) -> Edge o t l) es
+
+connect :: Edge e -> Graph v e -> Maybe (Graph v e)
+connect e g = if any (\v -> id v == origin e) (vertices g) &&
+                 any (\v -> id v == target e) (vertices g)
+              then Just (addEdge e g)
+              else Nothing
+
+disconnect :: Edge e -> Graph v e -> Maybe (Graph v e)
+disconnect e g = if e `elem` (edges g)
+                 then Just (removeEdge e g)
+                 else Nothing
+
+addEdge :: Edge e -> Graph v e -> Graph v e
+addEdge e g = Graph {
+                vertices = vertices g
+              , edges = e : edges g }
+
+addEdges :: [Edge e] -> Graph v e -> Graph v e
+addEdges es g = foldl (\g e -> addEdge e g) g es
+
+removeEdge :: Edge e -> Graph v e -> Graph v e
+removeEdge e g = Graph (vertices g) (filter (\e1 -> e /= e1) (edges g))
+
+removeEdges :: [Edge e] -> Graph v e -> Graph v e
+removeEdges es g = foldl (\g e -> removeEdge e g) g es
+
+addVertex :: Vertex v -> Graph v e -> Graph v e
+addVertex v g = Graph {
+                  vertices = v : vertices g
+                , edges = edges g }
+
+identityEdges :: Graph v e -> [Edge e]
+identityEdges g = map (\v -> Edge (id v) (id v) None) (vertices g)
+
+addIdentityEdges :: Graph v e -> Graph v e
+addIdentityEdges g = addEdges (identityEdges g) g
+
+removeIdentityEdges :: Graph v e -> Graph v e
+removeIdentityEdges g = removeEdges (identityEdges g) g
+
+
+
+{---------------------------------- Testing -----------------------------------}
+g1 = mkGraph' vs es
+    where vs = [Label 'A', Label 'B', Label 'C']
+          es = [(1,2,None),(2,3,None)]
+g2 = mkGraph' vs es
+    where vs = [Label 'X', Label 'Y', Label 'Z']
+          es = [(1,2,None),(2,3,None)]
