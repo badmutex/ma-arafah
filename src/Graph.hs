8bfb8476dd53eb8752f2d94007fe2bc0a2d57ac0
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-03-28 22:32:52 -0700

    [Graph] modular product of graphs implemented

--- src/Graph.hs
+++ src/Graph.hs
@@ -53,8 +53,8 @@ mkGraph vs es = Graph vs es
 
 mkGraph' :: [Label v] -> [(ID,ID,Label e)] -> Graph v e
 mkGraph' vs es = mkGraph vs' es'
-    where vs' = snd $ mapAccumL (\acc v -> (acc + 1,Vertex acc v)) 1 vs
-          es' = map (\(o,t,l) -> Edge o t l) es
+    where vs' = [ Vertex id v | id        <- [1.. ] | v <- vs]
+          es' = [ Edge o t l  | (o, t, l) <- es              ]
 
 connect :: Edge e -> Graph v e -> Maybe (Graph v e)
 connect e g = if any (\v -> id v == origin e) (vertices g) &&
@@ -81,6 +81,21 @@ removeEdge e g = Graph (vertices g) (filter (\e1 -> e /= e1) (edges g))
 removeEdges :: [Edge e] -> Graph v e -> Graph v e
 removeEdges es g = foldl (\g e -> removeEdge e g) g es
 
+connectionExists :: ID -> ID -> [Edge e] -> Bool
+connectionExists o t es = any match es || any tmatch es
+    where match e  = o == origin e && t == target e
+          tmatch e = o == target e && t == origin e
+
+
+connectionExists' :: Edge e -> Graph v e -> Bool
+connectionExists' e g = connectionExists (origin e) (target e) (edges g)
+
+connectionExists'' :: ID -> ID -> Graph v e -> Bool
+connectionExists'' o t g = connectionExists o t (edges g)
+
+connectionExists''' :: Edge e -> [Edge e] -> Bool
+connectionExists''' (Edge o t _) es = connectionExists o t es
+
 addVertex :: Vertex v -> Graph v e -> Graph v e
 addVertex v g = Graph {
                   vertices = v : vertices g
@@ -95,12 +110,54 @@ addIdentityEdges g = addEdges (identityEdges g) g
 removeIdentityEdges :: Graph v e -> Graph v e
 removeIdentityEdges g = removeEdges (identityEdges g) g
 
+removeDuplicateEdges :: [Edge e] -> [Edge e]
+removeDuplicateEdges es = foldl check [] es
+    where check es e = if not $ connectionExists''' e es then e : es else es
 
 
-{---------------------------------- Testing -----------------------------------}
+
+(<>) :: (Eq v, Eq v1) =>
+        Graph v e -> Graph v1 e1 -> Graph (Vertex v, Vertex v1) e2
+
+g1 <> g2 = let g1' = g1 --addIdentityEdges g1
+               g2' = g2 --addIdentityEdges g2
+               vs = [ Vertex id (Label (v1, v2)) |
+                      v1 <- vertices g1',
+                      v2 <- vertices g2'          |
+                      id <- [1..] ]
+
+               es = removeDuplicateEdges
+                    [ Edge (id v1) (id v2) None  |
+
+                      v1 <- vs,
+                      v2 <- vs,
+                      v1 /= v2,
+
+                      (     exist v1 g1'  &&      exist v2 g2') ||
+                      (not (exist v1 g1') && not (exist v2 g2')) ]
+
+                   where exist v g = connectionExists'' (nestedO v) (nestedT v) g
+
+                         nested (Vertex _ (Label l)) = l
+                         nestedO = id . fst . nested
+                         nestedT = id . snd . nested
+
+           in mkGraph vs es
+
+modularProduct :: (Eq v, Eq v1) =>
+                  Graph v e -> Graph v1 e1 -> Graph (Vertex v, Vertex v1) e2
+modularProduct = (<>)
+
+
+
+{-------------------------------- Testing & misc -------------------------------}
 g1 = mkGraph' vs es
     where vs = [Label 'A', Label 'B', Label 'C']
           es = [(1,2,None),(2,3,None)]
 g2 = mkGraph' vs es
     where vs = [Label 'X', Label 'Y', Label 'Z']
           es = [(1,2,None),(2,3,None)]
+
+pprint :: Show a => [a] -> IO ()
+pprint = mapM_ (putStrLn . show)
+
