8b0e054acc93de13573063bac6429f8510ddea53
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-04-26 20:45:56 -0700

    added cdk libraries and docs

---
+++ src/java/tajmi/src/tajmi/data/clusterable/cdk/Molecule_DistanceAlgorithm.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tajmi.data.clusterable.cdk;
+
+import org.openscience.cdk.interfaces.IMolecule;
+import tajmi.data.clusterable.DistanceAlgorithm;
+
+/**
+ *
+ * @author badi
+ */
+public class Molecule_DistanceAlgorithm implements DistanceAlgorithm<IMolecule> {
+
+    public double distance(IMolecule me, IMolecule you) {
+        
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+
+}
