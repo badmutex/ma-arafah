742f7b1b616ba93775c89000c22a846ede361f36
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-04-27 12:14:46 -0700

    [code] added clustering abilities for numerical vectors

---
+++ src/java/tajmi/src/tajmi/data/clusterable/num/Vector.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package tajmi.data.clusterable.num;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import scala.Tuple2;
+import tajmi.Util;
+
+/**
+ * Vector of numerical data to be clustered
+ * @author badi
+ */
+public class Vector extends ArrayList implements List, Iterable {
+
+    public Vector(){ super(); }
+    public Vector(int size){ super(size); }
+    public Vector(Collection c){ super(c); }
+
+    public Vector sum(Vector other){
+        Vector result = new Vector(Math.min(this.size(), other.size()));
+        for(Tuple2<Double,Double> tup : Util.zip(this, other)){
+            result.add(tup._1() + tup._2());
+        }
+        return result;
+    }
+}
