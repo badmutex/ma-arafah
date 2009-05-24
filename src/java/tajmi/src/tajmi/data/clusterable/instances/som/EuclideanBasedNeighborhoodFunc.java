10776f2a2daecaa17c64f870e366c8fc11b0e3d6
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-05-24 12:30:39 -0700

    Euclidean-based neighborhood function implemented

---
+++ src/java/tajmi/src/tajmi/data/clusterable/instances/som/EuclideanBasedNeighborhoodFunc.java
@@ -0,0 +1,33 @@
+
+package tajmi.data.clusterable.instances.som;
+
+import tajmi.data.clusterable.instances.num.som.PositionDistanceFunc;
+import tajmi.data.clusterable.interfaces.som.NeighborhoodFunc;
+import tajmi.data.som.Position;
+
+/**
+ *
+ * @author badi
+ */
+public class EuclideanBasedNeighborhoodFunc implements NeighborhoodFunc {
+
+    Position first, second;
+    double restraint;
+
+    public NeighborhoodFunc params(Position first, Position second, double restraint) {
+        this.first = first;
+        this.second = second;
+        this.restraint = restraint;
+        return this;
+    }
+
+    public Double call() {
+        if (first.equals(second))
+            return 1.0;
+        else {
+            double dist = new PositionDistanceFunc().params(first, second).call();
+            return (1.0 / dist) * restraint;
+        }
+    }
+
+}
