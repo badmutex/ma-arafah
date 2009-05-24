34b7af6580b4248c550010100b4bf76b5f6df43f
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-05-24 12:25:39 -0700

    added equals and hashCode methods for Position

--- src/java/tajmi/src/tajmi/data/som/Position.java
+++ src/java/tajmi/src/tajmi/data/som/Position.java
@@ -24,4 +24,21 @@ public class Position {
     public int y() {
         return y;
     }
+
+    @Override
+    public boolean equals (Object o) {
+        if (o instanceof Position) {
+            Position p = (Position) o;
+            Position t = this;
+            return (t.x() == p.x() && t.y() == p.y());
+        } else return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + this.x;
+        hash = 67 * hash + this.y;
+        return hash;
+    }
 }
