1eb1bb33357bc24c02a103491f745d4da6c42fab
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-05-30 19:09:00 -0700

    The vector projection function may be general enough to be generalized with a type parameter. Adjusted SOMMaker accordingly and added comments

--- src/java/tajmi/src/tajmi/frontends/SOMMaker.java
+++ src/java/tajmi/src/tajmi/frontends/SOMMaker.java
@@ -1,4 +1,3 @@
-
 package tajmi.frontends;
 
 import java.util.List;
@@ -6,7 +5,6 @@ import java.util.Random;
 import tajmi.instances.vectorial.Vector;
 import tajmi.instances.vectorial.VectorDistanceFunc;
 import tajmi.instances.vectorial.som.VectorInitFunc;
-import tajmi.instances.vectorial.som.VectorProjectionFunc;
 import tajmi.instances.vectorial.som.VectorStopFunc;
 import tajmi.instances.vectorial.som.VectorUpdateFunc;
 import tajmi.instances.som.NaiveFindBestMatchFunc;
@@ -18,6 +16,7 @@ import tajmi.abstracts.som.ProjectionFunc;
 import tajmi.abstracts.som.ShowStatusFunc;
 import tajmi.abstracts.som.StopFunc;
 import tajmi.abstracts.som.UpdateFunc;
+import tajmi.instances.som.GeneralProjectionFunc;
 import tajmi.som.Field;
 import tajmi.som.SOM;
 import tajmi.som.SOMParams;
@@ -31,9 +30,7 @@ public class SOMMaker<T> {
 
     SOMParams<T> params;
     List<T> data = null;
-
     int field_len, field_width;
-
     FindBestMatchFunc<T> find_bmu_func;
     InitFunc<T> init_func;
     NeighborhoodFunc neighborhood_func;
@@ -42,14 +39,14 @@ public class SOMMaker<T> {
     UpdateFunc<T> update_func;
     ShowStatusFunc show_status_func;
 
-    public SOMMaker () {
+    public SOMMaker() {
 
         show_status_func = new SimpleShowStatusFunc();
 
         params = new SOMParams<T>();
 
-        
-        params.iterations = 0;
+
+        params.iterations = 100;
         params.learning_restraint = 0.1;
         params.random_gen = new Random(42);
         params.restraint_modifier = 0.01;
@@ -61,13 +58,13 @@ public class SOMMaker<T> {
 
     }
 
-    public SOMMaker<T> randomSeed (long seed) {
+    public SOMMaker<T> randomSeed(long seed) {
         params.random_gen = new Random(seed);
-        
+
         return this;
     }
 
-    public SOMMaker<T> field_size (int len, int width) {
+    public SOMMaker<T> field_size(int len, int width) {
         field_len = len;
         field_width = width;
 
@@ -102,9 +99,7 @@ public class SOMMaker<T> {
         this.update_func = update_func;
     }
 
-
-
-    private SOM<T> makeSOM (List data) {
+    private SOM<T> makeSOM(List data) {
         assert data != null : "Cannot create a SOM on empty data";
 
         Field<T> field = new Field<T>(field_len, field_width, data, params.random_gen, init_func);
@@ -115,19 +110,42 @@ public class SOMMaker<T> {
         return new SOM<T>(data, params);
     }
 
-    public SOM<Vector> makeVectorialSOM (List<T> data) {
+    /**
+     * Creates a SOM over vectorial data using defaults if various functions have not been set: <br> <br>
+     * <code>
+     * projection_func = GeneralProjectionFunc <br>
+     * find_best_match = NaiveFindBestMatchFunc <br>
+     * update_func = VectorUpdateFunc <br>
+     * show_status_func = SimpleShowStatusFunc <br> <br>
+     * iterations = 100 <br>
+     * learning_restraint = 0.1 <br>
+     * random_gen = Random(42) <br>
+     * restraint_modifier = 0.01 <br>
+     * field_len = 50 <br>
+     * field_width = 50 <br>
+     * </code>
+     * @param data a sequence of data that the SOM should be trained with
+     * @return a SOM over vectorial data
+     */
+    public SOM<Vector> makeVectorialSOM(List<T> data) {
 
-        VectorProjectionFunc projectf = new VectorProjectionFunc();
+        if (params.project_func == null) {
+            ProjectionFunc<Vector> projectf = new GeneralProjectionFunc<Vector>();
 
-        projectf.setDistanceFunc(new VectorDistanceFunc());
-        projectf.setFindBestMatchFunc(new NaiveFindBestMatchFunc<Vector>());
-        projectf.setUpdateFunc(new VectorUpdateFunc());
+            projectf.setDistanceFunc(new VectorDistanceFunc());
+            projectf.setFindBestMatchFunc(new NaiveFindBestMatchFunc<Vector>());
+            projectf.setUpdateFunc(new VectorUpdateFunc());
 
-        params.project_func = (ProjectionFunc<T>) projectf;
+            params.project_func = (ProjectionFunc<T>) projectf;
+        }
 
-        params.stop_func = (StopFunc<T>) new VectorStopFunc();
+        if (params.stop_func == null) {
+            params.stop_func = (StopFunc<T>) new VectorStopFunc();
+        }
 
-        init_func = (InitFunc<T>) new VectorInitFunc();
+        if (init_func == null) {
+            init_func = (InitFunc<T>) new VectorInitFunc();
+        }
 
         return (SOM<Vector>) makeSOM(data);
     }
--- src/java/tajmi/src/tajmi/instances/som/GeneralProjectionFunc.java
+++ src/java/tajmi/src/tajmi/instances/som/GeneralProjectionFunc.java
@@ -1,8 +1,6 @@
 
-package tajmi.instances.vectorial.som;
+package tajmi.instances.som;
 
-import scala.Tuple2;
-import tajmi.instances.vectorial.Vector;
 import tajmi.abstracts.DistanceFunc;
 import tajmi.abstracts.som.FindBestMatchFunc;
 import tajmi.abstracts.som.ProjectionFunc;
@@ -10,36 +8,36 @@ import tajmi.abstracts.som.UpdateFunc;
 import tajmi.som.Field;
 import tajmi.som.Position;
 import tajmi.som.SOMParams;
-
+import scala.Tuple2;
 /**
  *
  * @author badi
  */
-public class VectorProjectionFunc extends ProjectionFunc<Vector> {
+public class GeneralProjectionFunc<T> extends ProjectionFunc<T> {
 
-    public SOMParams<Vector> call() {
+    @Override
+    public SOMParams<T> call() {
+        SOMParams<T> state = getState();
+        T datum = getDatum();
 
-        SOMParams<Vector> state = getState();
-        Vector datum = getDatum();
-
-        FindBestMatchFunc<Vector> find_best_match = getFindBestMatchFunc();
-        DistanceFunc<Vector> distancef = getDistanceFunc();
-        UpdateFunc<Vector> updatef = getUpdateFunc();
+        FindBestMatchFunc<T> find_best_match = getFindBestMatchFunc();
+        DistanceFunc<T> distancef = getDistanceFunc();
+        UpdateFunc<T> updatef = getUpdateFunc();
 
 
         /* implementation: */
 
         // 1) find the best matching unit
-        Tuple2<Position, Vector> res = find_best_match.params(state.field, datum, distancef).call();
+        Tuple2<Position, T> res = find_best_match.params(state.field, datum, distancef).call();
         Position best_pos = res._1();
 
         // 2) update the field weights using the restraint due to distance and time
-        Field<Vector> new_field = updatef.params(state.field, datum, best_pos, state.learning_restraint).call();
+        Field<T> new_field = updatef.params(state.field, datum, best_pos, state.learning_restraint).call();
 
         double new_restraint = state.learning_restraint * state.restraint_modifier;
 
         // 3) done. copy into a new state to return
-        SOMParams<Vector> new_state = state.copy();
+        SOMParams<T> new_state = state.copy();
         new_state.field = new_field;
         new_state.learning_restraint = new_restraint;
 
