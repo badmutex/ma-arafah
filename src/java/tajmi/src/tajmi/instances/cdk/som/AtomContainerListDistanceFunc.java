b1160c8e76cbef46410eaaf7dbff29eadf56334c
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-05-31 13:54:28 -0700

    Added an instance of distance func to compute the distance between an IAtomContainer and a list thereof.
    Given an IAtomContainer m and a list of IAtomContainers L, the distance is given by
    summing the distances between M and each m in L.

---
+++ src/java/tajmi/src/tajmi/instances/cdk/som/AtomContainerListDistanceFunc.java
@@ -0,0 +1,30 @@
+
+package tajmi.instances.cdk.som;
+
+import java.util.List;
+import org.openscience.cdk.interfaces.IAtomContainer;
+import tajmi.abstracts.DistanceFunc;
+import tajmi.instances.cdk.AtomContainerDistanceFunc;
+
+/**
+ * Given an IAtomContainer <code>M</code> and a list of IAtomContainers <code>L</code>,
+ * return the sum of the distances between <code>M</code> and each <code>m</code> in <code>L</code>.
+ * @author badi
+ */
+public class AtomContainerListDistanceFunc extends DistanceFunc<IAtomContainer, List<IAtomContainer>> {
+
+    @Override
+    public Double call() {
+        IAtomContainer molecule = getFirst();
+        List<IAtomContainer> others = getSecond();
+
+        DistanceFunc<IAtomContainer, IAtomContainer> distancef = new AtomContainerDistanceFunc();
+
+        double distance = 0.0;
+        for (IAtomContainer other : others)
+            distance += distancef.params(molecule, other).call();
+
+        return distance;
+    }
+
+}
