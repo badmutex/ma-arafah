0ba1c3fca4f1f72ab786840b70b5c34fad928abd
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-05-31 20:17:28 -0700

    working on AtomContainerUpdateFunc

--- src/java/tajmi/src/tajmi/instances/cdk/som/AtomContainerUpdateFunc.java
+++ src/java/tajmi/src/tajmi/instances/cdk/som/AtomContainerUpdateFunc.java
@@ -2,6 +2,7 @@ package tajmi.instances.cdk.som;
 
 import org.openscience.cdk.interfaces.IAtomContainer;
 import java.util.List;
+import tajmi.abstracts.som.NeighborhoodFunc;
 import tajmi.abstracts.som.UpdateFunc;
 import tajmi.som.Field;
 import tajmi.som.Position;
@@ -12,20 +13,24 @@ import tajmi.som.Position;
  */
 public class AtomContainerUpdateFunc extends UpdateFunc<List<IAtomContainer>, IAtomContainer> {
 
-    @Override
+    double distance_cutoff = 0.5;
+
+    public void setDistance_cutoff(double distance_cutoff) {
+        this.distance_cutoff = distance_cutoff;
+    }
+
+    @Override // TODO: finish AtomContainerUpdateFunc
     public Field<List<IAtomContainer>> call() {
         Field<List<IAtomContainer>> field = getField();
         IAtomContainer datum = getDatum();
         Position bmu_pos = getBestMatchingUnitPosition();
+        NeighborhoodFunc neighbourhoodf = getNeighborhoodFunc();
 
+        // 1) add datum to field under best matching unit
         field.get(bmu_pos).add(datum);
+
+        
 
         return field;
     }
-
-
-
-
-
-
 }
