90615bebc74a6a7c2b76b54d4abb7291743ba9e3
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-06-16 14:16:14 -0400

    abstract class DistanceFunc extends the Fun interface

--- src/java/tajmi/src/tajmi/abstracts/DistanceFunc.java
+++ src/java/tajmi/src/tajmi/abstracts/DistanceFunc.java
@@ -1,13 +1,16 @@
 package tajmi.abstracts;
 
 import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import tajmi.functional.interfaces.Fun;
 
 /**
  * The type parameters <code>A</code> and <code>B</code>
  * are the types of the instances to have a distance computed between them.
  * @author badi
  */
-public abstract class DistanceFunc<A,B> implements Callable<Double> {
+public abstract class DistanceFunc<A,B> implements Fun {
 
     A first;
     B second;
@@ -23,6 +26,31 @@ public abstract class DistanceFunc<A,B> implements Callable<Double> {
     public DistanceFunc params(A first, B second) {
         this.first = first;
         this.second = second;
+
+        return this;
+    }
+
+    public Fun copy() {
+        DistanceFunc d = null;
+        try {
+            d = (DistanceFunc) this.getClass().newInstance().curry(first).curry(second);
+        } catch (InstantiationException ex) {
+            Logger.getLogger(DistanceFunc.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            System.exit(-1);
+        } catch (IllegalAccessException ex) {
+            Logger.getLogger(DistanceFunc.class.getName()).log(Level.SEVERE, null, ex);
+            ex.printStackTrace();
+            System.exit(-1);
+        }
+        return d;
+    }
+
+    public Fun curry(Object arg) {
+        if (first == null)
+            first = (A) arg;
+        else if (second == null)
+            second = (B) arg;
 
         return this;
     }
