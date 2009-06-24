0e4d998725dec42648c0abd77806f505bcc92231
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-06-24 11:43:25 -0700

    Working SOM on molecules

--- src/java/tajmi/src/tajmi/TestSOM.java
+++ src/java/tajmi/src/tajmi/TestSOM.java
@@ -3,19 +3,21 @@ package tajmi;
 import java.io.File;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
 import scala.Tuple2;
+import tajmi.Settings.Variables;
 import tajmi.abstracts.som.ViewField;
 import tajmi.frontends.SOMMaker;
 import tajmi.functional.instances.cdk.ReadMolecule;
+import tajmi.functional.instances.cdk.WriteMolecule;
 import tajmi.instances.cdk.som.AtomContainerGrainyParallelListDistanceFunc;
-import tajmi.instances.cdk.som.AtomContainerParallelListDistanceFunc;
 import tajmi.instances.cdk.som.FieldModel;
 import tajmi.instances.cdk.som.OpenGLFieldView;
+import tajmi.instances.cdk.som.WriteClusterCenters;
 import tajmi.som.Field;
-import tajmi.som.Position;
 import tajmi.som.SOM;
 import tajmi.som.StatusUpdater;
 
@@ -35,7 +37,7 @@ public class TestSOM {
 
         public void TestIAtomContainerSOM() throws Exception {
             SOM som = moleculeSOM();
-            Field<IAtomContainer> f = som.call();
+            Field<FieldModel<IAtomContainer>> f = som.call();
             for (Tuple2 model : f) {
                 FieldModel<IAtomContainer> m = (FieldModel<IAtomContainer>) model._2();
                 if (m.getGeneralizeMedian() != null) System.out.println(m.getGeneralizeMedian().getID());
@@ -44,6 +46,21 @@ public class TestSOM {
             ViewField viewer = new OpenGLFieldView();
             viewer.params(f);
             viewer.call();
+//            Object o = new CollectClusterCenters().curry(f).call();
+            Map<Settings.Variables, Object> settings = new Settings(TEST_CONFIG_FILE).get_configuration();
+            Boolean write_mcss = (Boolean) settings.get(Settings.Variables.write_cluster_mcss);
+            if (write_mcss) {
+                String output_dir = (String) settings.get(Settings.Variables.cluster_mcss_directory);
+                String prefix = (String) settings.get(Variables.mcss_prefix);
+                WriteClusterCenters write = new WriteClusterCenters();
+                write.setOutput_directory(output_dir);
+                write.setOutput_prefix(prefix);
+                WriteMolecule w = (WriteMolecule) new WriteMolecule().curry(settings.get(Variables.mcss_format));
+                write.setMoleculeWriter(w);
+
+                write.params(f);
+                write.call();
+            }
 
             System.out.println("TestIAtomContainerSOM");
         }
@@ -54,7 +71,7 @@ public class TestSOM {
             sommaker.setMaxSOMIterations(1);
 //            sommaker.setDistance_func(new AtomContainerGrainyParallelListDistanceFunc());
 //            sommaker.setDistance_func(new AtomContainerParallelListDistanceFunc());
-            StatusUpdater.getInstance().set_verbosity_level(StatusUpdater.Verbosity.Verbose);
+            StatusUpdater.getInstance().set_verbosity_level(StatusUpdater.Verbosity.Everything);
 
             List<IMolecule> data = readMolecules();
             SOM som = sommaker.makeIAtomContainerSOM(data);
