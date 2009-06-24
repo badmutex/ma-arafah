1874439fce4a0cf1a4ef926ae8c8befa7029f17c
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2009-06-24 11:37:49 -0700

    Center of Mass function now implements Fun instead of Callable

--- src/java/tajmi/src/tajmi/abstracts/CenterOfMassFunc.java
+++ src/java/tajmi/src/tajmi/abstracts/CenterOfMassFunc.java
@@ -1,13 +1,13 @@
 
 package tajmi.abstracts;
 
-import java.util.concurrent.Callable;
+import tajmi.functional.interfaces.Fun;
 
 /**
  *
  * @author badi
  */
-public abstract class CenterOfMassFunc<T> implements Callable<T> {
+public abstract class CenterOfMassFunc<T> implements Fun {
 
     Iterable<T> cluster;
 
@@ -19,6 +19,22 @@ public abstract class CenterOfMassFunc<T> implements Callable<T> {
         this.cluster = cluster;
         return this;
     }
+
+    public Fun copy() {
+        try{
+            return this.getClass().newInstance().curry(cluster);
+        } catch (Exception ex) {
+            throw new RuntimeException(this.getClass() + " failed to copy itself", ex);
+        }
+    }
+
+    public Fun curry(Object arg) {
+        if (cluster == null)
+            cluster = (Iterable<T>) arg;
+
+        return this;
+    }
+
 
     public abstract T call ();
 }
